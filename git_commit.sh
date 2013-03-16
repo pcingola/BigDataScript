3457da0abb741ce3f60c301983a196f8604a8f3f
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2013-03-16 19:10:46 -0400

    Html pages updated

--- git_commit.sh
+++ git_commit.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/sh -e
 
 git add .
 git commit -a -m "Html pages updated"
 git push origin gh-pages
 
+echo
+echo
+echo
 echo "Pages:"
 echo "   http://pcingola.github.com/BigDataScript/"
