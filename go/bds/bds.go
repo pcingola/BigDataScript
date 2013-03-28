c736338c2123ca03362a0eddcf5cce905d75e5e4
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2013-03-28 03:02:26 -0400

    Project updated

--- go/bds/bds.go
+++ go/bds/bds.go
@@ -407,7 +407,11 @@ func killAll(pidFile string) {
 	// Run all commands
 	for cmd, args := range runCmds {
 		fmt.Printf("RUN:\t%s\t%s\n", cmd, args)
-		exec.Command("touch", args).Run()
+		cmdExec := exec.Command("touch", args)
+		err := cmdExec.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
