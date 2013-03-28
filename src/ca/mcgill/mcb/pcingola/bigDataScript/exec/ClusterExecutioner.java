22a53c4ef73f9be57d9c93757c7f444490e7b434
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2013-03-28 02:39:38 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/exec/ClusterExecutioner.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/exec/ClusterExecutioner.java
@@ -6,7 +6,6 @@ import ca.mcgill.mcb.pcingola.bigDataScript.cluster.Cluster;
 import ca.mcgill.mcb.pcingola.bigDataScript.cluster.host.HostResources;
 import ca.mcgill.mcb.pcingola.bigDataScript.osCmd.CmdRunner;
 import ca.mcgill.mcb.pcingola.bigDataScript.osCmd.CmdRunnerCluster;
-import ca.mcgill.mcb.pcingola.bigDataScript.util.Gpr;
 import ca.mcgill.mcb.pcingola.bigDataScript.util.Timer;
 
 /**
@@ -18,8 +17,8 @@ import ca.mcgill.mcb.pcingola.bigDataScript.util.Timer;
  */
 public class ClusterExecutioner extends LocalExecutioner {
 
-	//public static String FAKE_CLUSTER = "";
-	public static String FAKE_CLUSTER = Gpr.HOME + "/workspace/BigDataScript/fakeCluster/";
+	public static String FAKE_CLUSTER = "";
+	// public static String FAKE_CLUSTER = Gpr.HOME + "/workspace/BigDataScript/fakeCluster/";
 	public static String CLUSTER_EXEC_COMMAND[] = { FAKE_CLUSTER + "qsub" };
 	public static String CLUSTER_KILL_COMMAND[] = { FAKE_CLUSTER + "qdel" };
 	public static String CLUSTER_BDS_COMMAND = "bds qexec ";
