50a58871fa0a02dc1f80af547db07572fde9842c
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2013-09-04 13:43:48 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/lang/StatementInclude.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/lang/StatementInclude.java
@@ -9,16 +9,21 @@ import ca.mcgill.mcb.pcingola.bigDataScript.util.CompilerMessage.MessageType;
 import ca.mcgill.mcb.pcingola.bigDataScript.util.CompilerMessages;
 
 /**
+ * Include statement: Get source from another file
  */
 public class StatementInclude extends Block {
 
 	String includedFilename;
 	File includedFile;
 
+	private static boolean isValidFileName(String fileName2) {
+		if (fileName2 == null || fileName2.length() == 0 || !fileName2.trim().equals(fileName2)) return false;
+		return true;
+	}
+
 	public StatementInclude(BigDataScriptNode parent, ParseTree tree) {
 		super(parent, tree);
 	}
-
 
 	@Override
 	protected void parse(ParseTree tree) {
@@ -26,36 +31,23 @@ public class StatementInclude extends Block {
 		// ----------file name: this is merely informational
 		includedFilename = tree.getChild(0).getChild(1).getText();
 		includedFilename = includedFilename.substring(1, includedFilename.length() - 1).trim();
-		File includedFile=new File(includedFilename);
+		File includedFile = new File(includedFilename);
 		File parentFile = getFile();
-		if(parentFile != null && !includedFile.isAbsolute()  ) 
-			includedFile=new File(parentFile.getParent(),includedFilename);
+		if (parentFile != null && !includedFile.isAbsolute()) includedFile = new File(parentFile.getParent(), includedFilename);
 		// -------------
 		setFile(includedFile);
 		super.parse(tree.getChild(0)); // the Block parses the stament
 	}
 
-
 	@Override
 	protected void typeCheck(Scope scope, CompilerMessages compilerMessages) {
-		if (!isValidFileName(includedFilename)) 
-			compilerMessages.add(this, "For loop condition must be a bool expression", MessageType.ERROR);
+		if (!isValidFileName(includedFilename)) compilerMessages.add(this, "For loop condition must be a bool expression", MessageType.ERROR);
 		super.typeCheck(scope, compilerMessages);
 	}
 
-
-	private static boolean isValidFileName(String fileName2) {
-		if(fileName2 == null || fileName2.length()==0 || !fileName2.trim().equals(fileName2)) return false;
-		// more checks?
-		return true;
-	}
-
-
 	@Override
 	public void typeChecking(Scope scope, CompilerMessages compilerMessages) {
-		// TODO Auto-generated method stub
 		super.typeChecking(scope, compilerMessages);
 	}
 
-	
 }
