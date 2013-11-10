65d65ba14090d650f81eaa2da0279ce9d3de70ca
Pablo Cingolani <pcingola@Pablos-MacBook-Pro.local>
2013-11-10 18:21:25 -0500

    Project updated

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-set -e
+#!/bin/sh -e
+
+BDS_HOME="$HOME/.bds"
 
 echo "Changing dir" `dirname $0`
 
@@ -7,7 +8,7 @@ echo "Changing dir" `dirname $0`
 cd `dirname $0` &&  cd ..
 
 # Create 'bds' dir
-mkdir -p $HOME/.bds 2> /dev/null
+mkdir -p "$BDS_HOME" 2> /dev/null
 
 # Build Jar file
 echo Building JAR file
@@ -21,13 +22,20 @@ go clean
 go build bds.go
 
 # Build binay (go executable + JAR file)
-cat bds $HOME/.bds/BigDataScript.jar > bds.bin
+cat bds "$BDS_HOME/BigDataScript.jar" > bds.bin
 mv bds.bin bds
 chmod a+x bds
+mv bds "$BDS_HOME"
 
 # Remove JAR file
-rm $HOME/.bds/BigDataScript.jar
+rm "$BDS_HOME/BigDataScript.jar"
 
-# Done
-echo "Binary created: $HOME/.bds/bds"
+# Binary installed
+echo "Binary created: $BDS_HOME/bds"
+
+# Copy 'include' dir
+echo
+echo "Copying include files"
+cd - > /dev/null
+cp -rvf include "$BDS_HOME"
 
