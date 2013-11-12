46bd9125b17820b5a3f7ac845066ca677835bc62
Pablo Cingolani <pcingola@Pablos-MacBook-Pro.local>
2013-11-11 21:11:52 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/Zzz.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/Zzz.java
@@ -1,31 +1,10 @@
 package ca.mcgill.mcb.pcingola.bigDataScript;
 
-import ca.mcgill.mcb.pcingola.bigDataScript.exec.Tail;
 import ca.mcgill.mcb.pcingola.bigDataScript.util.Gpr;
 
 public class Zzz {
 
 	public static void main(String[] args) throws Exception {
-		Gpr.debug("Begin");
-		String tailDir = Gpr.HOME + "/tail";
-
-		int maxFiles = 1000;
-
-		Tail tail = new Tail();
-		tail.start();
-
-		Thread.sleep(1000);
-
-		for (int i = 0; i < maxFiles; i++) {
-			String file = tailDir + "/" + i + ".txt";
-
-			if (file.endsWith(".txt")) {
-				Gpr.debug("Adding file : " + file);
-				tail.add(file, null, false);
-			}
-		}
-
-		while (true)
-			Thread.sleep(1000);
+		Gpr.debug("PATH: " + System.getenv("PATH"));
 	}
 }
