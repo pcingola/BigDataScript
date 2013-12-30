0b6c0b8656e0b2d9903590abae144777ac5bc375
Pablo Cingolani <pcingola@Pablos-MacBook-Pro.local>
2013-12-29 20:16:08 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/lang/Continue.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/lang/Continue.java
@@ -2,8 +2,8 @@ package ca.mcgill.mcb.pcingola.bigDataScript.lang;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import ca.mcgill.mcb.pcingola.bigDataScript.compile.CompilerMessages;
 import ca.mcgill.mcb.pcingola.bigDataScript.compile.CompilerMessage.MessageType;
+import ca.mcgill.mcb.pcingola.bigDataScript.compile.CompilerMessages;
 import ca.mcgill.mcb.pcingola.bigDataScript.run.BigDataScriptThread;
 import ca.mcgill.mcb.pcingola.bigDataScript.run.RunState;
 import ca.mcgill.mcb.pcingola.bigDataScript.scope.Scope;
@@ -34,6 +34,9 @@ public class Continue extends Statement {
 
 	@Override
 	protected void typeCheck(Scope scope, CompilerMessages compilerMessages) {
-		if ((findParent(ForLoop.class, FunctionDeclaration.class) == null) && (findParent(While.class, FunctionDeclaration.class) == null)) compilerMessages.add(this, "break statement outside loop", MessageType.ERROR);
+		if ((findParent(ForLoop.class, FunctionDeclaration.class) == null) //
+				&& (findParent(ForLoopList.class, FunctionDeclaration.class) == null) //
+				&& (findParent(While.class, FunctionDeclaration.class) == null) //
+		) compilerMessages.add(this, "continue statement outside loop", MessageType.ERROR);
 	}
 }
