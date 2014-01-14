7369950971a72f6aac6c94bb5ea9375296263ca1
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-01-14 11:32:10 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/Zzz.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/Zzz.java
@@ -6,10 +6,5 @@ public class Zzz {
 
 	public static void main(String[] args) throws Exception {
 		Gpr.debug("PATH: " + System.getenv("PATH"));
-
-		String s;
-		String ss[] = { "hi", "bye" };
-		for (s : ss)
-			System.out.println(i);
 	}
 }
