30c1780051db78ea3440ed1a89da009c8c78a11d
Pablo Cingolani <pcingola@Pablos-MacBook-Pro.local>
2014-06-16 13:29:34 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
@@ -63,9 +63,9 @@ public class BigDataScript {
 	}
 
 	public static final String SOFTWARE_NAME = BigDataScript.class.getSimpleName();
-	public static final String BUILD = "2014-06-09";
+	public static final String BUILD = "2014-06-16";
 	public static final String REVISION = "";
-	public static final String VERSION_MAJOR = "0.97";
+	public static final String VERSION_MAJOR = "0.98";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 
 	public static final String VERSION = SOFTWARE_NAME + " " + VERSION_SHORT + " (build " + BUILD + "), by " + Pcingola.BY;
