247f77c7f318c53ea842fbd84fd02efe3d4341fe
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-22 10:14:25 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/test/TestCasesRun2.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/test/TestCasesRun2.java
@@ -167,20 +167,20 @@ public class TestCasesRun2 extends TestCasesBase {
 				+ "Iter 5, Task 1: End\n" //
 				+ "Iter 5, Task 2: Start\n" //
 				+ "Iter 5, Task 2: End\n" //
-		;;
+				;;
 
-		String stdout = runAndReturnStdout("test/run_117.bds");
+				String stdout = runAndReturnStdout("test/run_117.bds");
 
-		if (stdout.indexOf(expectedStdout) < 0) {
-			String msg = "Cannot find desired output:\n" //
-					+ "---------- Expected output ----------\n" //
-					+ expectedStdout //
-					+ "-------------- STDOUT --------------\n" //
-					+ stdout //
-			;
-			System.err.println(msg);
-			throw new RuntimeException(msg);
-		}
+				if (stdout.indexOf(expectedStdout) < 0) {
+					String msg = "Cannot find desired output:\n" //
+							+ "---------- Expected output ----------\n" //
+							+ expectedStdout //
+							+ "-------------- STDOUT --------------\n" //
+							+ stdout //
+							;
+					System.err.println(msg);
+					throw new RuntimeException(msg);
+				}
 	}
 
 	@Test
@@ -200,11 +200,11 @@ public class TestCasesRun2 extends TestCasesBase {
 				+ "Running task\n" //
 				+ "Creating tmp_out.txt\n" //
 				+ "Done\n" //"
-		;
+				;
 
 		String expectedStdout2 = "Running task\n" //
 				+ "Done\n" //"
-		;
+				;
 
 		System.out.println("First run:");
 		runAndCheckStdout("test/run_119.bds", expectedStdout1);
@@ -221,6 +221,11 @@ public class TestCasesRun2 extends TestCasesBase {
 	@Test
 	public void test121_split_fail_regex() {
 		runAndCheck("test/run_121.bds", "len", "1");
+	}
+
+	@Test
+	public void test122_nestest_break_continue() {
+		runAndCheck("test/run_122.bds", "out", "5\t7");
 	}
 
 }
