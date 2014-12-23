fe539c1119dcff5599030488064af1815253ba3a
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-23 10:43:24 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/test/TestCasesZzz.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/test/TestCasesZzz.java
@@ -1,6 +1,5 @@
 package ca.mcgill.mcb.pcingola.bigDataScript.test;
 
-import org.junit.Test;
 
 /**
  * Quick test cases when creating a new feature...
@@ -9,21 +8,5 @@ import org.junit.Test;
  *
  */
 public class TestCasesZzz extends TestCasesBase {
-
-	@Test
-	public void test34() {
-		verbose = true;
-		runAndCheck("test/run_34.bds", "exitStat", "0");
-	}
-
-	//	@Test
-	//	public void test15_checkpoint_par_function_call() {
-	//		runAndCheckpoint("test/checkpoint_15.bds", "test/checkpoint_15.chp", "ok", "true");
-	//	}
-	//
-	//	@Test
-	//	public void test16_checkpoint_recursive() {
-	//		runAndCheckpoint("test/checkpoint_16.bds", "test/checkpoint_16.chp", "fn", "120");
-	//	}
 
 }
