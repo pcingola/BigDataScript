d47f7aff089750b3f518737e9c32b19425c11661
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-17 09:26:33 -0500

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/lang/nativeMethods/string/MethodNative_string_replace_regex_repl.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/lang/nativeMethods/string/MethodNative_string_replace_regex_repl.java
@@ -1,16 +1,9 @@
 package ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.string;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-
-import ca.mcgill.mcb.pcingola.bigDataScript.task.Task;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.Parameters;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.Type;
-import ca.mcgill.mcb.pcingola.bigDataScript.lang.TypeList;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.MethodNative;
 import ca.mcgill.mcb.pcingola.bigDataScript.run.BigDataScriptThread;
-import ca.mcgill.mcb.pcingola.bigDataScript.util.Gpr;
 
 public class MethodNative_string_replace_regex_repl extends MethodNative {
 	public MethodNative_string_replace_regex_repl() {
@@ -19,7 +12,7 @@ public class MethodNative_string_replace_regex_repl extends MethodNative {
 
 	@Override
 	protected void initMethod() {
-		functionName = "replace";
+		functionName = "replaceAll";
 		classType = Type.STRING;
 		returnType = Type.STRING;
 
--- src/ca/mcgill/mcb/pcingola/bigDataScript/lang/nativeMethods/string/MethodNative_string_replace_str1_str2.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/lang/nativeMethods/string/MethodNative_string_replace_str1_str2.java
@@ -1,16 +1,9 @@
 package ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.string;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-
-import ca.mcgill.mcb.pcingola.bigDataScript.task.Task;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.Parameters;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.Type;
-import ca.mcgill.mcb.pcingola.bigDataScript.lang.TypeList;
 import ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.MethodNative;
 import ca.mcgill.mcb.pcingola.bigDataScript.run.BigDataScriptThread;
-import ca.mcgill.mcb.pcingola.bigDataScript.util.Gpr;
 
 public class MethodNative_string_replace_str1_str2 extends MethodNative {
 	public MethodNative_string_replace_str1_str2() {
@@ -31,6 +24,6 @@ public class MethodNative_string_replace_str1_str2 extends MethodNative {
 
 	@Override
 	protected Object runMethodNative(BigDataScriptThread csThread, Object objThis) {
-		return objThis.toString().replace( csThread.getString("str1"), csThread.getString("str2") );
+		return objThis.toString().replace(csThread.getString("str1"), csThread.getString("str2"));
 	}
 }
