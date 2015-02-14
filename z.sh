af2184927c7ef5e7c7a4d8513d0cb0940f1efd7a
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-02-14 13:05:02 -0500

    Project updated

--- z.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp src/ca/mcgill/mcb/pcingola/bigDataScript/SummaryTemplate.html bin/ca/mcgill/mcb/pcingola/bigDataScript
-cp src/ca/mcgill/mcb/pcingola/bigDataScript/DagTaskTemplate.js bin/ca/mcgill/mcb/pcingola/bigDataScript
-./scripts/clean.sh 
-
