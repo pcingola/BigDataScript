1646aae92303b98745298bf28b25904616d58577
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-02-14 00:41:00 -0500

    Added task / thread DAG in summary

--- src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
@@ -67,8 +67,8 @@ public class BigDataScript {
 	}
 
 	public static final String SOFTWARE_NAME = BigDataScript.class.getSimpleName();
-	public static final String BUILD = "2015-02-13";
-	public static final String REVISION = "e";
+	public static final String BUILD = "2015-02-14";
+	public static final String REVISION = "f";
 	public static final String VERSION_MAJOR = "0.999";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 
