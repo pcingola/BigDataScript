33801965bc1c1bd1293fab9d7e8587492743314d
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-05-14 14:48:04 -0400

    Project updated

--- scripts/distro.sh
+++ scripts/distro.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -e
 
-
 cd `dirname $0`
 dir=`pwd -P`
 cd -
 
+# Install files
 $dir/install.sh
+cp -vf config/bds.config $HOME/.bds/	# Make sure default config file is used
 
 cd $HOME
 tar -cvzf $dir/../distro/bds_`uname`.tgz .bds
