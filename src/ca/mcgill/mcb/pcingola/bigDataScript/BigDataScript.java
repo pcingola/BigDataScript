222aac136bca94c4afca9456289d4e66f25bfc8b
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-07-16 11:16:47 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/BigDataScript.java
@@ -69,7 +69,7 @@ public class BigDataScript {
 
 	public static final String SOFTWARE_NAME = BigDataScript.class.getSimpleName();
 	public static final String BUILD = "2015-07-16";
-	public static final String REVISION = "m";
+	public static final String REVISION = "l";
 	public static final String VERSION_MAJOR = "0.999";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 
