51ae494b87e981ceabff3d55a3a2ed1246562e25
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-07-20 17:04:14 -0400

    Project updated

---
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/lang/nativeMethods/string/MethodNative_string_download.java
@@ -0,0 +1,32 @@
+package ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.string;
+
+import ca.mcgill.mcb.pcingola.bigDataScript.data.Data;
+import ca.mcgill.mcb.pcingola.bigDataScript.lang.Parameters;
+import ca.mcgill.mcb.pcingola.bigDataScript.lang.Type;
+import ca.mcgill.mcb.pcingola.bigDataScript.lang.nativeMethods.MethodNative;
+import ca.mcgill.mcb.pcingola.bigDataScript.run.BdsThread;
+
+public class MethodNative_string_download extends MethodNative {
+	public MethodNative_string_download() {
+		super();
+	}
+
+	@Override
+	protected void initMethod() {
+		functionName = "download";
+		classType = Type.STRING;
+		returnType = Type.STRING;
+
+		String argNames[] = { "this" };
+		Type argTypes[] = { Type.STRING };
+		parameters = Parameters.get(argTypes, argNames);
+		addNativeMethodToClassScope();
+	}
+
+	@Override
+	protected Object runMethodNative(BdsThread bdsThread, Object objThis) {
+		Data data = bdsThread.data(objThis.toString());
+		if (!data.download()) return "";
+		return data.getLocalPath();
+	}
+}
