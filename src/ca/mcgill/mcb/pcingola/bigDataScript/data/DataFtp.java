ca4c0d5c586d5e4b74765de1cc1ed653bb029e1e
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-08-03 15:03:34 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/data/DataFtp.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/data/DataFtp.java
@@ -9,6 +9,9 @@ import ca.mcgill.mcb.pcingola.bigDataScript.util.Timer;
 
 /**
  * A file / directory on an ftp server
+ * 
+ * TODO: Use Apache's FTPClient instead of FtpURLConnection
+ * References: http://commons.apache.org/proper/commons-net/apidocs/org/apache/commons/net/ftp/FTPClient.html
  *
  * @author pcingola
  */
