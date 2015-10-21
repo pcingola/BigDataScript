9dd69e43ced0b4f98253c6306b8c136896d7e4ba
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-10-21 14:11:58 -0400

    Project updated

--- src/ca/mcgill/mcb/pcingola/bigDataScript/Bds.java
+++ src/ca/mcgill/mcb/pcingola/bigDataScript/Bds.java
@@ -69,7 +69,7 @@ public class Bds {
 	}
 
 	public static final String SOFTWARE_NAME = Bds.class.getSimpleName();
-	public static final String BUILD = "2015-09-29";
+	public static final String BUILD = "2015-09-21";
 	public static final String REVISION = "";
 	public static final String VERSION_MAJOR = "0.99999";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
