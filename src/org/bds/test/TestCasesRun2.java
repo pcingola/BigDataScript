6c68f7880a6d94381fd96268b8729eba70897989
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-02-17 18:18:32 -0500

    Updated test case for linux dev

--- src/org/bds/test/TestCasesRun2.java
+++ src/org/bds/test/TestCasesRun2.java
@@ -356,24 +356,30 @@ public class TestCasesRun2 extends TestCasesBase {
 	public void test126_task_dependency_scheduled() {
 		Gpr.debug("Test");
 
-		String expectedOutput = "IN: /Users/pcingola/zzz/in.txt\n" //
-				+ "OUT: /Users/pcingola/zzz/out.txt\n" //
-				+ "OUT_0: /Users/pcingola/zzz/out_0.txt\n" //
-				+ "OUT_1: /Users/pcingola/zzz/out_1.txt\n" //
-				+ "OUT_2: /Users/pcingola/zzz/out_2.txt\n" //
-				+ "    OUT_0_0: /Users/pcingola/zzz/out_0_0.txt\n" //
-				+ "    OUT_0_1: /Users/pcingola/zzz/out_0_1.txt\n" //
-				+ "    OUT_0_2: /Users/pcingola/zzz/out_0_2.txt\n" //
-				+ "    OUT_1_0: /Users/pcingola/zzz/out_1_0.txt\n" //
-				+ "    OUT_1_1: /Users/pcingola/zzz/out_1_1.txt\n" //
-				+ "    OUT_1_2: /Users/pcingola/zzz/out_1_2.txt\n" //
-				+ "    OUT_2_0: /Users/pcingola/zzz/out_2_0.txt\n" //
-				+ "    OUT_2_1: /Users/pcingola/zzz/out_2_1.txt\n" //
-				+ "    OUT_2_2: /Users/pcingola/zzz/out_2_2.txt\n" //
+		String expectedOutput = "IN: " + Gpr.HOME + "/zzz/in.txt\n" //
+				+ "OUT: " + Gpr.HOME + "/zzz/out.txt\n" //
+				+ "OUT_0: " + Gpr.HOME + "/zzz/out_0.txt\n" //
+				+ "    OUT_0_0: " + Gpr.HOME + "/zzz/out_0_0.txt\n" //
+				+ "    OUT_0_1: " + Gpr.HOME + "/zzz/out_0_1.txt\n" //
+				+ "    OUT_0_2: " + Gpr.HOME + "/zzz/out_0_2.txt\n" //
+				+ "OUT_1: " + Gpr.HOME + "/zzz/out_1.txt\n" //
+				+ "    OUT_1_0: " + Gpr.HOME + "/zzz/out_1_0.txt\n" //
+				+ "    OUT_1_1: " + Gpr.HOME + "/zzz/out_1_1.txt\n" //
+				+ "    OUT_1_2: " + Gpr.HOME + "/zzz/out_1_2.txt\n" //
+				+ "OUT_2: " + Gpr.HOME + "/zzz/out_2.txt\n" //
+				+ "    OUT_2_0: " + Gpr.HOME + "/zzz/out_2_0.txt\n" //
+				+ "    OUT_2_1: " + Gpr.HOME + "/zzz/out_2_1.txt\n" //
+				+ "    OUT_2_2: " + Gpr.HOME + "/zzz/out_2_2.txt\n" //
 				;
 
 		String stdout = runAndReturnStdout("test/run_126.bds");
-		if (verbose) System.err.println("STDOUT: " + stdout);
+		if (verbose) {
+			System.err.println("STDOUT:" //
+					+ "\n----------------------------------------\n" //
+					+ stdout //
+					+ "\n----------------------------------------" //
+			);
+		}
 
 		// Check that task output lines
 		for (String out : expectedOutput.split("\n")) {
