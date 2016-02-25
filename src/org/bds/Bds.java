c091618deb3f3961c0f69f700eb3321bc4a3ce48
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-02-25 16:39:20 -0500

    Improved resource memory management on local host

--- src/org/bds/Bds.java
+++ src/org/bds/Bds.java
@@ -60,7 +60,7 @@ public class Bds {
 	}
 
 	public static final String SOFTWARE_NAME = Bds.class.getSimpleName();
-	public static final String BUILD = "2016-02-20";
+	public static final String BUILD = "2016-02-25";
 	public static final String REVISION = "c";
 	public static final String VERSION_MAJOR = "0.99999";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
