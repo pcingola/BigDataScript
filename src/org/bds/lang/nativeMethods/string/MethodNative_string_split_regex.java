0a91de5d1b0da80935696972577fceaacc0dd071
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-07-20 07:54:10 -0400

    list.split() method uses Java's split with limit=-1

--- src/org/bds/lang/nativeMethods/string/MethodNative_string_split_regex.java
+++ src/org/bds/lang/nativeMethods/string/MethodNative_string_split_regex.java
@@ -1,16 +1,12 @@
 package org.bds.lang.nativeMethods.string;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import org.bds.lang.Parameters;
 import org.bds.lang.Type;
 import org.bds.lang.TypeList;
 import org.bds.lang.nativeMethods.MethodNative;
 import org.bds.run.BdsThread;
-import org.bds.task.Task;
-import org.bds.util.Gpr;
 
 public class MethodNative_string_split_regex extends MethodNative {
 	public MethodNative_string_split_regex() {
@@ -31,6 +27,15 @@ public class MethodNative_string_split_regex extends MethodNative {
 
 	@Override
 	protected Object runMethodNative(BdsThread csThread, Object objThis) {
-		String str = objThis.toString(); if( str.isEmpty() ) return new ArrayList<String>(); try { return array2list( str.split( csThread.getString("regex") ) ); } catch( Throwable t ) { ArrayList<String> l = new ArrayList<String>(); l.add(str); return l; }
+		String str = objThis.toString();
+		if (str.isEmpty()) return new ArrayList<String>();
+		try {
+			String regex = csThread.getString("regex");
+			return array2list(str.split(regex, -1));
+		} catch (Throwable t) {
+			ArrayList<String> l = new ArrayList<>();
+			l.add(str);
+			return l;
+		}
 	}
 }
