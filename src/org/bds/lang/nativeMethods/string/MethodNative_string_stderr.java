69614190e692cb15af0bafdbb79adb9f3a158f90
Jin Lee <leepc12@gmail.com>
2016-08-23 08:46:01 -0700

    fix taskId.isDone()

--- src/org/bds/lang/nativeMethods/string/MethodNative_string_stderr.java
+++ src/org/bds/lang/nativeMethods/string/MethodNative_string_stderr.java
@@ -4,6 +4,7 @@ import org.bds.lang.Parameters;
 import org.bds.lang.Type;
 import org.bds.lang.nativeMethods.MethodNative;
 import org.bds.run.BdsThread;
+import org.bds.run.BdsThreads;
 import org.bds.task.Task;
 import org.bds.util.Gpr;
 
@@ -27,7 +28,7 @@ public class MethodNative_string_stderr extends MethodNative {
 	@Override
 	protected Object runMethodNative(BdsThread bdsThread, Object objThis) {
 		String taskId = objThis.toString();
-		Task task = bdsThread.getTask(taskId);
+		Task task = BdsThreads.getTask(taskId);
 		if (task == null) return "";
 		return Gpr.readFile(task.getStderrFile(), false);
 	}
