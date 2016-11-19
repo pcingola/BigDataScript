fd4fbe8eccfaaa5593959778f0ceb6cb20448184
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-11-19 10:29:27 -0500

    Project updated

--- src/org/bds/Bds.java
+++ src/org/bds/Bds.java
@@ -63,7 +63,7 @@ public class Bds {
 
 	public static final String SOFTWARE_NAME = Bds.class.getSimpleName();
 	public static final String BUILD = Gpr.compileTimeStamp(Bds.class);
-	public static final String REVISION = "e";
+	public static final String REVISION = "f";
 	public static final String VERSION_MAJOR = "0.99999";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 
