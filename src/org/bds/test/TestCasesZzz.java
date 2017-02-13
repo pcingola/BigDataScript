8fbd353f90dbbe0d77dd136e658337e8e92864df
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2017-02-13 11:23:16 -0500

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -12,45 +12,10 @@ import org.junit.Test;
 public class TestCasesZzz extends TestCasesBase {
 
 	@Test
-	public void test145_switch() {
+	public void test11() {
 		Gpr.debug("Test");
-		runAndCheck("test/run_145.bds", "out", 3);
-	}
-
-	@Test
-	public void test146_switch_fallthrough() {
-		Gpr.debug("Test");
-		runAndCheck("test/run_146.bds", "out", 35);
-	}
-
-	@Test
-	public void test147_switch_default() {
-		Gpr.debug("Test");
-		runAndCheck("test/run_147.bds", "out", 100);
-	}
-
-	@Test
-	public void test148_switch_default_fallthrough() {
-		Gpr.debug("Test");
-		runAndCheck("test/run_148.bds", "out", 700);
-	}
-
-	@Test
-	public void test24_switch() {
-		Gpr.debug("Test");
-		runAndCheckpoint("test/checkpoint_24.bds", "test/checkpoint_24.chp", "out", 103);
-	}
-
-	@Test
-	public void test25_switch() {
-		Gpr.debug("Test");
-		runAndCheckpoint("test/checkpoint_25.bds", "test/checkpoint_25.chp", "out", 35);
-	}
-
-	@Test
-	public void test26_switch() {
-		Gpr.debug("Test");
-		runAndCheckpoint("test/checkpoint_26.bds", "test/checkpoint_26.chp", "out", 56);
+		// Run pipeline and test checkpoint
+		runAndCheckpoint("test/checkpoint_11.bds", "test/checkpoint_11.chp", "sumPar", "110");
 	}
 
 }
