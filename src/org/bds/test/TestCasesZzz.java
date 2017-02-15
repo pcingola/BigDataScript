a5aca6dc353b6e633806afb9a95d48d2273b75c9
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2017-02-15 12:17:59 -0500

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -3,6 +3,8 @@ package org.bds.test;
 import org.bds.util.Gpr;
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 /**
  * Quick test cases when creating a new feature...
  *
@@ -12,10 +14,29 @@ import org.junit.Test;
 public class TestCasesZzz extends TestCasesBase {
 
 	@Test
-	public void test11() {
+	public void test124_quiet_mode() {
 		Gpr.debug("Test");
-		// Run pipeline and test checkpoint
-		runAndCheckpoint("test/checkpoint_11.bds", "test/checkpoint_11.chp", "sumPar", "110");
+		String output = "print 0\n" //
+				+ "print 1\n" //
+				+ "print 2\n" //
+				+ "print 3\n" //
+				+ "print 4\n" //
+				+ "print 5\n" //
+				+ "print 6\n" //
+				+ "print 7\n" //
+				+ "print 8\n" //
+				+ "print 9\n" //
+				;
+
+		// Run and capture stdout
+		String args[] = { "-quiet" };
+		String stdout = runAndReturnStdout("test/run_124.bds", args);
+		if (verbose) System.err.println("STDOUT: " + stdout);
+
+		// Check that sys and task outputs are not there
+		Assert.assertTrue("Print output should be in STDOUT", stdout.contains(output));
+		Assert.assertTrue("Task output should NOT be in STDOUT", !stdout.contains("task"));
+		Assert.assertTrue("Sys output should NOT be in STDOUT", !stdout.contains("sys"));
 	}
 
 }
