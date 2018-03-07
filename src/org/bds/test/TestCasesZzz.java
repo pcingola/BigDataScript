10b620725bf0c1303c81d42cae0a48b699342643
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-03-06 21:36:09 -0500

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,5 +1,8 @@
 package org.bds.test;
 
+import org.bds.util.Gpr;
+import org.junit.Test;
+
 /**
  * Quick test cases when creating a new feature...
  *
@@ -7,5 +10,23 @@ package org.bds.test;
  *
  */
 public class TestCasesZzz extends TestCasesBase {
+
+	@Test
+	public void test03() {
+		Gpr.debug("Test");
+		runAndCheckpoint("test/graph_03.bds", "test/graph_03.chp", "out", "Task start\nTask end\n");
+	}
+
+	@Test
+	public void test04() {
+		Gpr.debug("Test");
+		runAndCheckpoint("test/graph_04.bds", "test/graph_04.chp", "out", "IN\nTASK 1\nTASK 2\n");
+	}
+
+	@Test
+	public void test10() {
+		Gpr.debug("Test");
+		runAndCheck("test/graph_10.bds", "num", "2");
+	}
 
 }
