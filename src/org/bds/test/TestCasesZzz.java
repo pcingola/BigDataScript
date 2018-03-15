1dc40dd29e8cec69103f30006f2472d20c9c7e8c
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-03-15 17:04:54 -0400

    ReferenceField works OK (reference an object field)

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,5 +1,8 @@
 package org.bds.test;
 
+import org.bds.util.Gpr;
+import org.junit.Test;
+
 /**
  * Quick test cases when creating a new feature...
  *
@@ -8,34 +11,52 @@ package org.bds.test;
  */
 public class TestCasesZzz extends TestCasesBase {
 
-	//	@Test
-	//	public void test200() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_200.bds", "", "");
-	//	}
-	//
-	//	@Test
-	//	public void test201() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_201.bds", "", "");
-	//	}
-	//
-	//	@Test
-	//	public void test202() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_202.bds", "", "");
-	//	}
-	//
-	//	@Test
-	//	public void test203() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_203.bds", "", "");
-	//	}
-	//
-	//	@Test
-	//	public void test204() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_204.bds", "", "");
-	//	}
+	@Test
+	public void test200() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_200.bds", "", "");
+	}
+
+	@Test
+	public void test201() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_201.bds", "", "");
+	}
+
+	@Test
+	public void test202() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_202.bds", "", "");
+	}
+
+	@Test
+	public void test203() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_203.bds", "", "");
+	}
+
+	@Test
+	public void test204() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_204.bds", "", "");
+	}
+
+	@Test
+	public void test205() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_205.bds", "", "");
+	}
+
+	@Test
+	public void test206() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_206.bds", "", "");
+	}
+
+	@Test
+	public void test207() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_207.bds", "", "");
+	}
 
 }
