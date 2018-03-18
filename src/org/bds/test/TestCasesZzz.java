4950362ebe9a6dfe437190f70e9d50a84c371d84
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-03-17 21:34:44 -0400

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,5 +1,8 @@
 package org.bds.test;
 
+import org.bds.util.Gpr;
+import org.junit.Test;
+
 /**
  * Quick test cases when creating a new feature...
  *
@@ -8,24 +11,12 @@ package org.bds.test;
  */
 public class TestCasesZzz extends TestCasesBase {
 
-	//	@Test
-	//	public void zrun_test50() {
-	//		Gpr.debug("Test");
-	//		HashMap<String, Object> expectedValues = new HashMap<>();
-	//		expectedValues.put("i", "32");
-	//		expectedValues.put("j", "302");
-	//		expectedValues.put("jx", "44");
-	//		expectedValues.put("jy", "91");
-	//
-	//		runAndCheck("test/run_50.bds", expectedValues);
-	//	}
-	//
-	//	@Test
-	//	public void zrun_test15_3() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_15.bds", "li3", "[apple, orange, 1]");
-	//	}
-	//
+	@Test
+	public void zrun_test15_3() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_15.bds", "li3", "[apple, orange, 1]");
+	}
+
 	//	@Test
 	//	public void zrun_test95() {
 	//		Gpr.debug("Test");
@@ -38,18 +29,7 @@ public class TestCasesZzz extends TestCasesBase {
 	//		String stdout = runAndReturnStdout("test/run_116.bds");
 	//		Assert.assertEquals("hi bye\nThe answer\t\tis: 42", stdout);
 	//	}
-	//
-	//	@Test
-	//	public void zrun_test140_list_nonvariable() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_140.bds", "i", "2");
-	//	}
-	//
-	//	@Test
-	//	public void zrun_test141_map_nonvariable() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_141.bds", "i", "42");
-	//	}
+
 	//
 	//	@Test
 	//	public void test200() {
