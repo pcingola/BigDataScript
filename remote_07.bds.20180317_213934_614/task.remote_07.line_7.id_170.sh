6fe1b34650df3bd276a7c5a5551dd50df41a58d3
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-03-19 11:25:30 -0400

    Project updated

--- remote_07.bds.20180317_213934_614/task.remote_07.line_7.id_170.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-cd '/home/pcingola/workspace/BigDataScript'
-# Download commands
-bds -download "http://pcingola.github.io/BigDataScript/index.html" "/tmp/bds/http/pcingola/github/io/BigDataScript/index.html"
-bds -download "http://pcingola.github.io/BigDataScript/about.html" "/tmp/bds/http/pcingola/github/io/BigDataScript/about.html"
- cat /tmp/bds/http/pcingola/github/io/BigDataScript/index.html /tmp/bds/http/pcingola/github/io/BigDataScript/about.html > tmp_remote_07.txt
-# Checksum: 613352aa
