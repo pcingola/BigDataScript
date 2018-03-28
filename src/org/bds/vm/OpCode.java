1b064e110a60372a72d42ea4bf0b3625a34a129b
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-03-28 10:03:15 -0400

    VM definitions

--- src/org/bds/vm/OpCode.java
+++ src/org/bds/vm/OpCode.java
@@ -1,60 +1,69 @@
 package org.bds.vm;
 
+/**
+ * The opcodes are stored into an array of Integers
+ *
+ * Literal always reference the pool of constants: bool, int, real, string or function
+ *
+ * Nomenclature:
+ *     literal  : An integer referencing a literal (bool|int|real|string) in the pool of constants
+ *     funcName : An integer referencing a string (the function's name) in the pool of constants
+ *     varName  : An integer referencing a string (the variable's name) in the pool of constants
+ *
+ * @author pcingola
+ */
 public enum OpCode {
-	ADDI // Addition (int)
-	, ADDR // Addition (real) 
-	, ADDS // Addition (string): Concatenate 
-	, ANDB // And (bool)
-	, ANDI // And (int)
-	, BTOI // bool to int
-	, BTOR // bool to real
-	, CALL // Call a function
-	, DIVI // Division (int)
-	, DIVR // Division (real)
-	, EQB // Equality (bool)
-	, EQI // Equality (int)
-	, EQR // Equality (real)
-	, EQS // Equality (string)
-	, GEB // Greater or equal than (bool)
-	, GEI // Greater or equal than (int)
-	, GER // Greater or equal than (real)
-	, GES // Greater or equal than (string)
-	, GTB // Greater than (bool)
-	, GTI // Greater than (int)
-	, GTR // Greater than (real)
-	, GTS // Greater than (string)
-	, HALT // Halt (stop execution in current thread)
-	, ITOB // int to bool
-	, ITOR // int to real
-	, JMP // Jump (inconditional)
-	, JMPT // Jump if true
-	, JMPF // Jump if false
-	, LOAD // Load from local scope
-	, LTB // Less than (bool)
-	, LTI // Less than (int)
-	, LTR // Less than (real)
-	, LTS // Less than (string)
-	, LEB // Less or equal than (bool)
-	, LEI // Less or equal than (int)
-	, LER // Less or equal than (real)
-	, LES // Less or equal than (string)
-	, MODI // Modulo (int)
-	, MULI // Multiplication (int)
-	, MULR // Multiplication (real)
-	, NOP // No operation
-	, ORB // Or (bool)
-	, ORI // Or (int)
-	, PUSHB // Push literal (bool)
-	, PUSHI // Push literal (int)
-	, PUSHR // Push literal (real)
-	, PUSHS // Push literal (string)
-	, RET // Return (from function)
-	, RTOB // real to bool
-	, RTOI // real to int
-	, STORE // Store in local scope
-	, SUBI // Subtraction (int) 
-	, SUBR // Subtraction (real)
-	, XORB // Xor (bool)
-	, XORI // Xor (int)
-
+	// Addition (int, real, string)
+	ADDI, ADDR, ADDS
+	// And: bool (logic), int (bitwise)
+	, ANDB, ANDI
+	// Cast values {b,i,r} => {b,i,r}
+	, CAST_BTOI, CAST_BTOR, CAST_ITOB, CAST_ITOR, CAST_RTOB, CAST_RTOI
+	// Function call:
+	//    CALL funcName
+	, CALL
+	// Division
+	, DIVI, DIVR
+	// Equality test
+	, EQB, EQI, EQR
+	// Greater or equal than
+	, GEB, GEI, GER, GES
+	// Greater than
+	, GTB, GTI, GTR
+	// Halt (stop execution in current thread)
+	, HALT
+	// Jumps: unconditional, jump if true, jump if false: 'JMP pc'
+	, JMP, JMPT, JMPF
+	// Load variable/array/dictionary local scope (into stack)
+	//    LOAD varName
+	//    LOADIDX varListName     # Use latest stack element as index
+	//    LOADDICT varDictName    # Use latest stack element as key
+	, LOAD, LOADIDX, LOADDICT
+	// Less than
+	, LTB, LTI, LTR, LTS
+	// Less or equal than
+	, LEB, LEI, LER, LES
+	// Modulo (int)
+	, MODI
+	// Multiplication (int)
+	, MULI, MULR
+	// No operation
+	, NOP
+	// OR: bool (logical), int (bitwise)
+	, ORB, ORI
+	// Push literal
+	//    PUSHNULL                 # Pushes a 'null' literal
+	//    PUSH{B|I|R|S}  literal   # Pushes a literal constant into the stack
+	, PUSHB, PUSHI, PUSHNULL, PUSHR, PUSHS
+	// Return (from function)
+	, RET
+	// Store variable/array/dictionary in local scope
+	//    STORE varName
+	//    STOREIDX varListName     # Use latest stack element as index
+	//    STOREDICT varDictName    # Use latest stack element as key
+	, STORE, STOREIDX, STOREDICT
+	// Subtraction
+	, SUBI, SUBR
+	// XOR
+	, XORB, XORI
 }
