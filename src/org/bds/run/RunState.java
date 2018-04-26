503335f79e73b5bac147424dab9178477a4afc45
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-04-26 17:24:50 -0400

    RunState 'WAIT_RECOVER' removed

--- src/org/bds/run/RunState.java
+++ src/org/bds/run/RunState.java
@@ -11,17 +11,7 @@ public enum RunState {
 	, FATAL_ERROR // Finished executing due to a fatal error
 	, FINISHED // Thread execution completely finished (all tasks and threads also finished execution)
 	, THREAD_KILLED // Thread was sent a 'kill' signal
-	, WAIT_RECOVER // Recovering from a checkpoint, in a "wait" instruction
 	;
-
-	//	/**
-	//	 * Are we in 'Checkpoint Recovery' mode?
-	//	 */
-	//	public boolean isCheckpointRecover() {
-	//		return this == RunState.WAIT_RECOVER //
-	//				|| this == RunState.CHECKPOINT_RECOVER //
-	//		;
-	//	}
 
 	public boolean isFatalError() {
 		return this == FATAL_ERROR;
