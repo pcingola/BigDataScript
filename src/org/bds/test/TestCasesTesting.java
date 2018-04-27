95dec2eb7729643e082883eaf4c451318f51e785
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-04-27 18:20:04 -0400

    Project updated

--- src/org/bds/test/TestCasesTesting.java
+++ src/org/bds/test/TestCasesTesting.java
@@ -13,31 +13,31 @@ import org.junit.Test;
  */
 public class TestCasesTesting extends TestCasesBase {
 
-	//	@Test
-	//	public void testTestCases01() {
-	//		Gpr.debug("Test");
-	//		runTestCasesPass("test/test_case_run_01.bds");
-	//	}
-	//
-	//	@Test
-	//	public void testTestCases02() {
-	//		Gpr.debug("Test");
-	//		runTestCasesPass("test/test_case_run_02.bds");
-	//	}
-	//
-	//	@Test
-	//	public void testTestCases03() {
-	//		Gpr.debug("Test");
-	//		verbose = true;
-	//		runTestCasesPass("test/test_case_run_03.bds");
-	//	}
-	//
-	//	@Test
-	//	public void testTestCases04() {
-	//		Gpr.debug("Test");
-	//		verbose = true;
-	//		runTestCasesPass("test/test_case_run_04.bds");
-	//	}
+	@Test
+	public void testTestCases01() {
+		Gpr.debug("Test");
+		runTestCasesPass("test/test_case_run_01.bds");
+	}
+
+	@Test
+	public void testTestCases02() {
+		Gpr.debug("Test");
+		runTestCasesPass("test/test_case_run_02.bds");
+	}
+
+	@Test
+	public void testTestCases03() {
+		Gpr.debug("Test");
+		verbose = true;
+		runTestCasesPass("test/test_case_run_03.bds");
+	}
+
+	@Test
+	public void testTestCases04() {
+		Gpr.debug("Test");
+		verbose = true;
+		runTestCasesPass("test/test_case_run_04.bds");
+	}
 
 	@Test
 	public void testTestCases05() {
