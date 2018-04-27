4fd8b6d0f5496245c66d9386fc3bfbe8f902c82d
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-04-27 18:14:03 -0400

    bds test improvements

--- src/org/bds/test/TestCasesTesting.java
+++ src/org/bds/test/TestCasesTesting.java
@@ -24,12 +24,26 @@ public class TestCasesTesting extends TestCasesBase {
 	//		Gpr.debug("Test");
 	//		runTestCasesPass("test/test_case_run_02.bds");
 	//	}
+	//
+	//	@Test
+	//	public void testTestCases03() {
+	//		Gpr.debug("Test");
+	//		verbose = true;
+	//		runTestCasesPass("test/test_case_run_03.bds");
+	//	}
+	//
+	//	@Test
+	//	public void testTestCases04() {
+	//		Gpr.debug("Test");
+	//		verbose = true;
+	//		runTestCasesPass("test/test_case_run_04.bds");
+	//	}
 
 	@Test
-	public void testTestCases03() {
+	public void testTestCases05() {
 		Gpr.debug("Test");
 		verbose = true;
-		runTestCasesPass("test/test_case_run_03.bds");
+		runTestCasesPass("test/test_case_run_05.bds");
 	}
 
 }
