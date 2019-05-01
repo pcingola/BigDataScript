58e3e7490690997b8295d9455490ca8f903af793
Pablo Cnigolani <pablo.e.cingolani@gmail.com>
2019-04-30 21:55:47 -0400

    Implemented FtpConnectionFactory

--- src/org/bds/data/DataFtp.java
+++ src/org/bds/data/DataFtp.java
@@ -1,13 +1,9 @@
 package org.bds.data;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 
-import org.apache.commons.net.ftp.FTPClient;
+//import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.bds.util.Gpr;
 import org.bds.util.Timer;
@@ -31,13 +27,6 @@ public class DataFtp extends DataRemote {
 		canWrite = false;
 	}
 
-	/**
-	 * Close connection
-	 */
-	protected void close() {
-		FtpConnectionFactory.get().close(uri);
-	}
-
 	@Override
 	public boolean delete() {
 		throw new RuntimeException("Unimplemented!");
@@ -50,34 +39,8 @@ public class DataFtp extends DataRemote {
 
 	@Override
 	public boolean download(String localFileName) {
-		FTPClient ftp = open();
-		OutputStream output = null;
-		try {
-			mkdirsLocal(localFileName);
-			output = new FileOutputStream(localFileName);
-		} catch (FileNotFoundException e1) {
-			String msg = "Error opening local file '" + localFileName + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e1);
-		}
-
-		String remotePath = getPath();
-		try {
-			ftp.retrieveFile(remotePath, output);
-		} catch (IOException e) {
-			String msg = "Error downloading file '" + remotePath + "' from host '" + hostname + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
-		} finally {
-			try {
-				output.close();
-			} catch (IOException e) {
-				String msg = "Error closing file '" + localFileName + "'";
-				Timer.showStdErr(msg);
-				throw new RuntimeException(msg, e);
-			}
-		}
-		close();
+		mkdirsLocal(localFileName);
+		FtpConnectionFactory.get().download(uri, localFileName);
 		return true;
 	}
 
@@ -93,18 +56,10 @@ public class DataFtp extends DataRemote {
 
 	@Override
 	public ArrayList<String> list() {
-		FTPClient ftp = open();
 		ArrayList<String> filesStr = new ArrayList<>();
-		try {
-			FTPFile[] files = ftp.listFiles(getPath());
-			for (FTPFile file : files)
-				filesStr.add(file.getName());
-		} catch (IOException e) {
-			String msg = "Error reading remote directory '" + getPath() + "', from FTP server '" + hostname + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
-		}
-		close();
+		FTPFile[] files = FtpConnectionFactory.get().list(uri);
+		for (FTPFile file : files)
+			filesStr.add(file.getName());
 		return filesStr;
 	}
 
@@ -116,80 +71,44 @@ public class DataFtp extends DataRemote {
 		throw new RuntimeException("Unimplemented!");
 	}
 
-	protected FTPClient open() {
-		return FtpConnectionFactory.get().open(uri);
-	}
-
 	@Override
 	protected boolean updateInfo() {
 		latestUpdate = new Timer(CACHE_TIMEOUT);
 		boolean ok = true;
-		FTPClient ftp = open();
-		String path = getPath();
-		Gpr.debug("path: " + path);
-		try {
-			FTPFile[] files = ftp.listFiles(path);
-			if (files == null || files.length < 1) {
-				// Path does not exists
-				size = -1;
-				canRead = false;
-				exists = false;
-				lastModified = new Date(0);
-			} else if (files.length == 1) {
-				// Single file
-				FTPFile file = files[0];
+		FTPFile[] files = FtpConnectionFactory.get().list(uri);
+		if (files == null || files.length < 1) {
+			// Path does not exists
+			size = -1;
+			canRead = false;
+			exists = false;
+			lastModified = new Date(0);
+		} else if (files.length == 1) {
+			// Single file
+			FTPFile file = files[0];
 
-				size = file.getSize();
-				canRead = true;
-				exists = true;
+			size = file.getSize();
+			canRead = true;
+			exists = true;
 
-				// Last modified
-				long epoch = file.getTimestamp().getTimeInMillis() / 1000;
-				lastModified = new Date(epoch);
-			} else {
-				// Directory
-				Gpr.debug("MULTIPLE FILES!");
-				long maxTimeStampMillis = 0;
-				for (FTPFile f : files) {
-					maxTimeStampMillis = Math.max(maxTimeStampMillis, f.getTimestamp().getTimeInMillis());
-				}
-
-				size = -1;
-				canRead = true;
-				exists = true;
-
-				// Last modified
-				long epoch = maxTimeStampMillis / 1000;
-				lastModified = new Date(epoch);
+			// Last modified
+			long epoch = file.getTimestamp().getTimeInMillis() / 1000;
+			lastModified = new Date(epoch);
+		} else {
+			// Directory
+			Gpr.debug("MULTIPLE FILES!");
+			long maxTimeStampMillis = 0;
+			for (FTPFile f : files) {
+				maxTimeStampMillis = Math.max(maxTimeStampMillis, f.getTimestamp().getTimeInMillis());
 			}
-		} catch (IOException e) {
-			String msg = "Error reading remote path '" + getPath() + "', from FTP server '" + hostname + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
-		}
 
-		//		if (connection == null) {
-		//			// Cannot connect
-		//			canRead = false;
-		//			exists = false;
-		//			lastModified = new Date(0);
-		//			size = 0;
-		//			ok = false;
-		//		} else {
-		//			// Update data
-		//			size = ftp..getContentLengthLong(); // Could be negative (unspecified)
-		//			canRead = true;
-		//			exists = true;
-		//
-		//			// Last modified
-		//			long lastMod = connection.getLastModified();
-		//			if (lastMod == 0) lastMod = connection.getDate(); // If last_modified is not found, use 'date' (e.g. dynamic content)
-		//			lastModified = new Date(lastMod);
-		//
-		//			ok = true;
-		//
-		//		}
-		close();
+			size = -1;
+			canRead = true;
+			exists = true;
+
+			// Last modified
+			long epoch = maxTimeStampMillis / 1000;
+			lastModified = new Date(epoch);
+		}
 
 		// Show information
 		if (debug) Timer.showStdErr("Updated infromation for '" + this + "'"//
--- src/org/bds/data/FtpConnectionFactory.java
+++ src/org/bds/data/FtpConnectionFactory.java
@@ -1,14 +1,22 @@
 package org.bds.data;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimerTask;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
+import org.bds.Config;
+import org.bds.util.Gpr;
 import org.bds.util.Timer;
 import org.bds.util.Tuple;
 
@@ -19,115 +27,175 @@ import org.bds.util.Tuple;
  * @author pcingola
  *
  */
-public class FtpConnectionFactory {
+public class FtpConnectionFactory extends TimerTask {
 
 	public static boolean verbose = true;
+	public static boolean debug = false;
+
+	public static int TIMER_DELAY = 1 * 1000;
+	public static int TIMER_PERIOD = 1 * 1000;
+	public static int MAX_CONNECTION_TIME = 5 * 1000;
+
 	private static FtpConnectionFactory instance = null;
+	private static java.util.Timer timer = null;
 
-	Map<String, FTPClient> ftpClientByHostName;
-	Map<String, Integer> usageCount;
+	Map<String, FTPClient> ftpClientByKey;
+	Map<String, Long> latestUsage;
+	boolean running;
 
+	/**
+	 * Get factory instance
+	 */
 	public static FtpConnectionFactory get() {
-		if (instance == null) instance = new FtpConnectionFactory();
+		if (instance == null) {
+			debug = Config.get().isDebug();
+			debug = true;
+			if (debug) Gpr.debug("Creating new FtpConnectionFactory instance");
+			instance = new FtpConnectionFactory();
+			// Initialize a timer thread to periodically review connections
+			timer = new java.util.Timer(instance.getClass().getName());
+			timer.schedule(instance, TIMER_DELAY, TIMER_PERIOD);
+		}
 		return instance;
 	}
 
+	/**
+	 * Kill instance
+	 */
+	public static void kill() {
+		if (debug) Gpr.debug("Killing FtpConnectionFactory timer");
+		if (instance != null) instance.running = false;
+		if (timer != null) timer.cancel();
+		timer = null;
+		instance = null;
+	}
+
 	private FtpConnectionFactory() {
-		ftpClientByHostName = new HashMap<>();
-		usageCount = new HashMap<>();
+		ftpClientByKey = new HashMap<>();
+		latestUsage = new HashMap<>();
+		running = true;
 	}
 
 	/**
 	 * Close a connection
 	 */
-	public void close(URI uri) {
-		String key = key(uri);
-		int count = dec(key);
-		if (count <= 0) {
-			FTPClient ftp = get(key);
-			try {
-				ftp.disconnect();
-			} catch (IOException e) {
-				Timer.showStdErr("ERROR while disconnecting from '" + uri.getHost() + "'");
-			}
-		}
+	public void close(String key) {
+		if (debug) Gpr.debug("FtpConnectionFactory: Closing connection '" + key + "'");
+		FTPClient ftp = delete(key);
+		if (ftp != null) disconnect(ftp);
 	}
 
 	/**
 	 * Connect to remote server
 	 */
 	private void connect(FTPClient ftp, URI uri) {
-		if (ftp != null && ftp.isConnected()) return;
+		if (debug) Gpr.debug("FtpConnectionFactory: Connecting to '" + uri + "'");
+		synchronized (ftp) {
+			if (ftp.isConnected()) return;
 
-		String hostname = uri.getHost();
+			String hostname = uri.getHost();
 
-		// Configure
-		FTPClientConfig config = new FTPClientConfig();
-		config.setLenientFutureDates(true);
-		ftp.configure(config);
+			// Configure
+			FTPClientConfig config = new FTPClientConfig();
+			config.setLenientFutureDates(true);
+			ftp.configure(config);
 
-		// Connect
-		try {
-			if (verbose) Timer.showStdErr("Connecting to '" + hostname + "'");
-			ftp.connect(hostname);
-			// After connection attempt, you should check the reply code to verify success.
-			int reply = ftp.getReplyCode();
+			// Connect
+			try {
+				if (verbose) Timer.showStdErr("Connecting to '" + hostname + "'");
+				ftp.connect(hostname);
+				// After connection attempt, you should check the reply code to verify success.
+				int reply = ftp.getReplyCode();
 
-			if (!FTPReply.isPositiveCompletion(reply)) {
-				ftp.disconnect();
-				String msg = "FTP Connection error, host '" + hostname + "', URL: '" + uri + "', reply code '" + reply + "'";
+				if (!FTPReply.isPositiveCompletion(reply)) {
+					ftp.disconnect();
+					String msg = "FTP Connection error, host '" + hostname + "', URL: '" + uri + "', reply code '" + reply + "'";
+					Timer.showStdErr(msg);
+					throw new RuntimeException(msg);
+				}
+			} catch (Exception e) {
+				String msg = "ERROR while connecting to '" + hostname + "'";
 				Timer.showStdErr(msg);
-				throw new RuntimeException(msg);
+				throw new RuntimeException(msg, e);
 			}
-		} catch (Exception e) {
-			String msg = "ERROR while connecting to '" + hostname + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
-		}
 
-		// Log into server
-		Tuple<String, String> userPass = setUserInfo(uri);
-		try {
-			ftp.login(userPass.first, userPass.second);
-		} catch (IOException e) {
-			String msg = "ERROR while logging into server '" + hostname + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
-		}
+			// Log into server
+			Tuple<String, String> userPass = setUserInfo(uri);
+			try {
+				ftp.login(userPass.first, userPass.second);
+			} catch (IOException e) {
+				String msg = "ERROR while logging into server '" + hostname + "'";
+				Timer.showStdErr(msg);
+				throw new RuntimeException(msg, e);
+			}
 
-		// Use binary mode
-		ftp.setControlKeepAliveTimeout(60);
-		try {
-			ftp.setFileTransferMode(FTP.BINARY_FILE_TYPE);
-		} catch (IOException e) {
-			String msg = "Unable to set FTP transfer to binary mode for host '" + hostname + "', URL: '" + uri + "'";
-			Timer.showStdErr(msg);
-			throw new RuntimeException(msg, e);
+			// Use binary mode
+			ftp.setControlKeepAliveTimeout(60);
+			try {
+				ftp.setFileTransferMode(FTP.BINARY_FILE_TYPE);
+			} catch (IOException e) {
+				String msg = "Unable to set FTP transfer to binary mode for host '" + hostname + "', URL: '" + uri + "'";
+				Timer.showStdErr(msg);
+				throw new RuntimeException(msg, e);
+			}
+		}
+		if (debug) Gpr.debug("FtpConnectionFactory: Connected to '" + uri + "'");
+	}
+
+	/**
+	 * Delete a key
+	 * @return Deleted FTPClient
+	 */
+	private synchronized FTPClient delete(String key) {
+		if (debug) Gpr.debug("FtpConnectionFactory: deleting key '" + key + "'");
+		FTPClient ftp = ftpClientByKey.remove(key);
+		latestUsage.remove(key);
+		return ftp;
+	}
+
+	/**
+	 * Disconnect and FTP client
+	 */
+	private void disconnect(FTPClient ftp) {
+		if (ftp == null) return;
+		synchronized (ftp) {
+			if (!ftp.isConnected()) return;
+			try {
+				ftp.disconnect();
+			} catch (IOException e) {
+				Gpr.debug("ERROR while disconnecting from ftp '" + ftp + "'");
+			}
 		}
 	}
 
 	/**
-	 * Decrement usage count, remove counter when non-positive
+	 * Download a file
 	 */
-	private synchronized int dec(String key) {
-		int count = usageCount.getOrDefault(key, 0) - 1;
-
-		if (count <= 0) usageCount.remove(key);
-		else usageCount.put(key, count);
-
-		return count;
-	}
-
-	private FTPClient get(String key) {
-		return ftpClientByHostName.get(key);
+	public boolean download(URI uri, String localFileName) {
+		FTPClient ftp = open(uri);
+		return retrieveFile(ftp, uri, localFileName);
 	}
 
 	/**
-	 * Increment usage count
+	 * Time elapsed since latest usage
 	 */
-	private synchronized void inc(String key) {
-		int count = usageCount.getOrDefault(key, 0);
-		usageCount.put(key, count + 1);
+	private synchronized long elapsed(String key) {
+		long now = (new Date()).getTime();
+		long latest = latestUsage.getOrDefault(key, 0L);
+		return now - latest;
+	}
+
+	/**
+	 * Get or create a new FTP client
+	 */
+	private synchronized FTPClient getOrCreateFtpClient(String key) {
+		FTPClient ftp = ftpClientByKey.get(key);
+		if (ftp == null) {
+			if (debug) Gpr.debug("FtpConnectionFactory: Creating FTP client for key '" + key + "'");
+			ftp = new FTPClient();
+			ftpClientByKey.put(key, ftp);
+		}
+		return ftp;
 	}
 
 	/**
@@ -139,19 +207,97 @@ public class FtpConnectionFactory {
 	}
 
 	/**
+	 * List files from URI
+	 */
+	public FTPFile[] list(URI uri) {
+		FTPClient ftp = open(uri);
+		return listFiles(ftp, uri);
+	}
+
+	/**
+	 * List files using an FTP client
+	 */
+	public FTPFile[] listFiles(FTPClient ftp, URI uri) {
+		synchronized (ftp) {
+			try {
+				return ftp.listFiles(uri.getPath());
+			} catch (IOException e) {
+				String msg = "Error reading remote directory '" + uri + "'";
+				Timer.showStdErr(msg);
+				throw new RuntimeException(msg, e);
+			}
+		}
+	}
+
+	/**
 	 * Create a new client (open a connection)
 	 * @return
 	 */
-	public FTPClient open(URI uri) {
+	private FTPClient open(URI uri) {
+		if (debug) Gpr.debug("FtpConnectionFactory: Openning FTP to '" + uri + "'");
 		String key = key(uri);
-		FTPClient ftp = get(key);
-		if (ftp == null) {
-			ftp = new FTPClient();
-			ftpClientByHostName.put(key, ftp);
-			connect(ftp, uri);
-		}
-		inc(key);
+		FTPClient ftp = getOrCreateFtpClient(key);
+		connect(ftp, uri);
+		update(key);
 		return ftp;
+	}
+
+	/**
+	 * Retrieve a remote file using FTPClient
+	 */
+	public boolean retrieveFile(FTPClient ftp, URI uri, String localFileName) {
+		synchronized (ftp) {
+			OutputStream output = null;
+			try {
+				output = new FileOutputStream(localFileName);
+			} catch (FileNotFoundException e1) {
+				String msg = "Error opening local file '" + localFileName + "'";
+				Timer.showStdErr(msg);
+				return false;
+			}
+
+			String remotePath = uri.getPath();
+			try {
+				ftp.retrieveFile(remotePath, output);
+			} catch (IOException e) {
+				String msg = "Error downloading file '" + remotePath + "' from host '" + uri.getHost() + "'";
+				Timer.showStdErr(msg);
+				return false;
+			} finally {
+				try {
+					output.close();
+				} catch (IOException e) {
+					String msg = "Error closing file '" + localFileName + "'";
+					Timer.showStdErr(msg);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Timer action: Review and cleanup ftp connections
+	 */
+	@Override
+	public void run() {
+		if (debug) Gpr.debug("FtpConnectionFactory cleanup");
+
+		for (String key : ftpClientByKey.keySet()) {
+			if (!running) {
+				if (debug) Gpr.debug("FtpConnectionFactory killed: Abort cleanup loop (run)");
+				return;
+			}
+
+			// Has the connection expired?
+			if (elapsed(key) > MAX_CONNECTION_TIME) close(key);
+		}
+
+		// If there are no further connections, this thread can be killed
+		if (ftpClientByKey.isEmpty()) {
+			if (debug) Gpr.debug("FtpConnectionFactory: No more ftp clients, killing timer task");
+			kill();
+		}
 	}
 
 	/**
@@ -169,6 +315,14 @@ public class FtpConnectionFactory {
 			password = (fields.length > 1 ? fields[1] : "");
 		}
 		return new Tuple<>(username, password);
+	}
+
+	/**
+	 * Increment usage count
+	 */
+	private synchronized void update(String key) {
+		long epoch = (new Date()).getTime();
+		latestUsage.put(key, epoch);
 	}
 
 }
