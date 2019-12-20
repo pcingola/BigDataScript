adbb8a9a6e36884fd4743bd2769b500450e08823
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-12-20 15:24:02 -0300

    Project updated

--- remote_08.bds.20190806_131239_206/task.remote_08.line_7.id_6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-set -o pipefail
-
-cd '/Users/kqrw311/workspace/BigDataScript'
-
-# SYS command. line 7
-cat http://pcingola.github.io/BigDataScript/index.html http://pcingola.github.io/BigDataScript/about.html > tmp_remote_08.txt
-# Checksum: 1ba56fdf
--- remote_18.bds.20190827_105524_108/task.remote_18.line_5.id_8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-set -o pipefail
-
-cd '/Users/kqrw311/workspace/BigDataScript'
-
-# SYS command. line 5
-echo Hi
-# Checksum: 7dc5de77
