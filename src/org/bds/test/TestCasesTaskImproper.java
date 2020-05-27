b4cf4f17c13f38ad32cf04c72504610c8a847a44
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-05-27 13:31:11 -0400

    Project updated

--- src/org/bds/test/TestCasesTaskImproper.java
+++ src/org/bds/test/TestCasesTaskImproper.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.bds.util.Gpr;
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 /**
  * Test cases Classes / Objects
  *
@@ -14,15 +16,15 @@ import org.junit.Test;
  */
 public class TestCasesTaskImproper extends TestCasesBase {
 
-	//	/**
-	//	 * Execute a task: Local computer
-	//	 */
-	//	@Test
-	//	public void test01() {
-	//		Gpr.debug("Test");
-	//		runAndCheck("test/run_task_improper_01.bds", "a", "42");
-	//	}
-	//
+	/**
+	 * Execute a task: Local computer
+	 */
+	@Test
+	public void test01() {
+		Gpr.debug("Test");
+		runAndCheck("test/run_task_improper_01.bds", "a", "42");
+	}
+
 	/**
 	 * Execute a task: Capture exit command, stdout, stderr
 	 */
@@ -38,25 +40,25 @@ public class TestCasesTaskImproper extends TestCasesBase {
 		runAndCheckStdout("test/run_task_improper_02.bds", expected, null, false);
 	}
 
-	//	/**
-	//	 * Execute two tasks: Make sure execution ends at task end and environment is inherited
-	//	 */
-	//	@Test
-	//	public void test03() {
-	//		Gpr.debug("Test");
-	//		verbose = true;
-	//		debug = true;
-	//		List<String> expected = new ArrayList<>();
-	//		expected.add("Task improper: Before, a=42");
-	//		for (int i = 0; i < 3; i++) {
-	//			expected.add("Task improper: Start, a=42, i=" + i);
-	//			expected.add("Task improper: End, a=" + (42 + i) + ", i=" + i);
-	//		}
-	//		expected.add("Task improper: After, a=42");
-	//
-	//		runAndCheckStdout("test/run_task_improper_03.bds", expected, null, false);
-	//	}
-	//
+	/**
+	 * Execute two tasks: Make sure execution ends at task end and environment is inherited
+	 */
+	@Test
+	public void test03() {
+		Gpr.debug("Test");
+
+		List<String> expected = new ArrayList<>();
+		expected.add("Task improper: Before, a=42");
+		for (int i = 0; i < 3; i++) {
+			expected.add("Task improper: Start, a=42, i=" + i);
+			expected.add("Task improper: End, a=" + (42 + i) + ", i=" + i);
+		}
+		expected.add("Task improper: After, a=42");
+
+		String stdout = runAndCheckStdout("test/run_task_improper_03.bds", expected, null, false);
+		Assert.assertTrue("Should finish with a 'Done' message", stdout.endsWith("Done\n"));
+	}
+
 	//	/**
 	//	 * Execute a task with remote files
 	//	 */
