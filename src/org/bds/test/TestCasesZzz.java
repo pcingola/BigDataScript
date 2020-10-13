4da25273dc6610e5292d559efdf892fdca9a8140
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-10-12 20:49:13 -0400

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,7 +1,5 @@
 package org.bds.test;
 
-import java.util.HashMap;
-
 import org.bds.Config;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,15 +18,36 @@ public class TestCasesZzz extends TestCasesBase {
 		Config.get().load();
 	}
 
+	// TODO: Task output in s3
 	@Test
-	public void test254_getvar() {
-		HashMap<String, Object> expectedValues = new HashMap<>();
+	public void test34() {
+		// TODO: Get bucket name from $BDS_HOME/aws_test_bucket.txt
+		runAndCheck("test/remote_34.bds", null);
+	}
 
-		expectedValues.put("shome", System.getenv().get("HOME"));
-		expectedValues.put("szzz", "VALUE_DEFAULT_2");
-		expectedValues.put("szzzxxxzzz", "VALUE_DEFAULT_3");
+	// TODO: Check task input in s3, output to s3
+	@Test
+	public void test36() {
+	}
 
-		runAndCheck("test/run_254.bds", expectedValues);
+	// TODO: Check task multiple outputs to s3
+	@Test
+	public void test37() {
+	}
+
+	// TODO: Check task multiple inputs and multiple outputs in s3
+	@Test
+	public void test38() {
+	}
+
+	// TODO: Check task local inputs, out1 -> out2 -> out3 in S3 (cached input for the next task)
+	@Test
+	public void test39() {
+	}
+
+	// TODO: Check task local inputs, out -> out -> out in S3 (detect an error or at least a WARNING?)
+	@Test
+	public void test40() {
 	}
 
 }
