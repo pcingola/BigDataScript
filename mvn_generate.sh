c6fe877168c76c905840bd2b9bd157a34dd4178e
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-10-14 20:34:44 -0400

    Project updated

--- mvn_generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mvn archetype:generate \
-	-DgroupId=org.bds \
-	-DartifactId=bds \
-	-DarchetypeArtifactId=maven-archetype-quickstart \
-	-DarchetypeVersion=1.4 \
-	-DinteractiveMode=false
