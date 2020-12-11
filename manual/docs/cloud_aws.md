7e39546492e235604d599ddea4042880a59dd2b3
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-12-11 16:28:09 -0500

    Project updated

---
+++ manual/docs/cloud_aws.md
@@ -0,0 +1,16 @@
+
+# Cloud: AWS
+
+
+### AWS Region lookup
+
+Search strategy
+- Resources ...
+- Variable `awsRegion`
+- Config
+- AWS `$HOME/.aws/config`  `..../credentials`
+
+WARNING: $HOME/.aws/config and credentials need to match, some AWS SDK and CLI programs might pick different (python vs java)
+
+
+
--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,11 +1,7 @@
 package org.bds.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bds.Config;
 import org.bds.util.Gpr;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,23 +19,30 @@ public class TestCasesZzz extends TestCasesBase {
 		Config.get().load();
 	}
 
-	/**
-	 * Execute several tasks within a 'par' function
-	 */
-	@Test
-	public void test06() {
-		Gpr.debug("Test");
-		verbose = true;
-		List<String> expected = new ArrayList<>();
-		expected.add("Task improper: Before, a=42");
-		for (int i = 0; i < 5; i++) {
-			expected.add("Task improper: Start, a=42, i=" + i);
-			expected.add("Task improper: End, a=" + (42 + i) + ", i=" + i);
-		}
-		expected.add("Task improper: After, a=42");
+	//	/**
+	//	 * Execute several tasks within a 'par' function
+	//	 */
+	//	@Test
+	//	public void test06() {
+	//		Gpr.debug("Test");
+	//		verbose = true;
+	//		List<String> expected = new ArrayList<>();
+	//		expected.add("Task improper: Before, a=42");
+	//		for (int i = 0; i < 5; i++) {
+	//			expected.add("Task improper: Start, a=42, i=" + i);
+	//			expected.add("Task improper: End, a=" + (42 + i) + ", i=" + i);
+	//		}
+	//		expected.add("Task improper: After, a=42");
+	//
+	//		String stdout = runAndCheckStdout("test/run_task_improper_06.bds", expected, null, false);
+	//		Assert.assertTrue("Should finish with a 'Done' message", stdout.endsWith("Done\n"));
+	//	}
 
-		String stdout = runAndCheckStdout("test/run_task_improper_06.bds", expected, null, false);
-		Assert.assertTrue("Should finish with a 'Done' message", stdout.endsWith("Done\n"));
+	// Create a remote checkpoint
+	@Test
+	public void test29() {
+		Gpr.debug("Test");
+		runAndCheck("test/checkpoint_29.bds", "ok", true);
 	}
 
 }
