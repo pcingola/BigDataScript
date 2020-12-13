400a6cf10b42a80f3070e4ad4910c2a2792f7edf
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-12-12 19:12:32 -0500

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -1,9 +1,7 @@
 package org.bds.test;
 
 import org.bds.Config;
-import org.bds.util.Gpr;
 import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Quick test cases when creating a new feature...
@@ -17,26 +15,6 @@ public class TestCasesZzz extends TestCasesBase {
 	public void beforeEachTest() {
 		Config.reset();
 		Config.get().load();
-	}
-
-	/**
-	 * 'dep' and 'goal' using improper tasks with taskId dependencies
-	 */
-	@Test
-	public void test09() {
-		Gpr.debug("Test");
-
-		String stdout = //
-				"Task improper: Start\n" //
-						+ "DEP_1: Start\n" //
-						+ "DEP_1: End\n" //
-						+ "DEP_2: Start\n" //
-						+ "DEP_2: End\n" //
-						+ "DEP_3: Start\n" //
-						+ "DEP_3: End\n" //
-						+ "Task improper: End\n";
-
-		runAndCheckStdout("test/run_task_improper_09.bds", stdout);
 	}
 
 }
