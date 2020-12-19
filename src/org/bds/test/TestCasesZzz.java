8cdaf94a3c0e1ce876e0cd0095750feec4ddf7b0
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-12-19 06:33:47 -0500

    Project updated

--- src/org/bds/test/TestCasesZzz.java
+++ src/org/bds/test/TestCasesZzz.java
@@ -2,7 +2,6 @@ package org.bds.test;
 
 import org.bds.Config;
 import org.bds.util.Gpr;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,17 +19,28 @@ public class TestCasesZzz extends TestCasesBase {
 		Config.get().load();
 	}
 
+	//	/**
+	//	 * Execute a task having an assignment sys instead of a bare sys
+	//	 * 	task(...) {
+	//	 * 		out = sys cat z.txt
+	//	 * 	}
+	//	 */
+	//	@Test
+	//	public void test04() {
+	//		Gpr.debug("Test");
+	//		String stdout = runAndCheckStdout("test/run_task_improper_04.bds", "Task improper: End, a=42, str='A=42'");
+	//		Assert.assertTrue("Should finish with a 'Done' message", stdout.endsWith("Done\n"));
+	//	}
+
 	/**
-	 * Execute a task having an assignment sys instead of a bare sys
-	 * 	task(...) {
-	 * 		out = sys cat z.txt
-	 * 	}
+	 * Test checkpoint during execution of two dependent tasks.
+	 * Must recover both tasks and their dependencies
 	 */
 	@Test
 	public void test04() {
 		Gpr.debug("Test");
-		String stdout = runAndCheckStdout("test/run_task_improper_04.bds", "Task improper: End, a=42, str='A=42'");
-		Assert.assertTrue("Should finish with a 'Done' message", stdout.endsWith("Done\n"));
+		verbose = true;
+		runAndCheckpoint("test/graph_04.bds", "test/graph_04.chp", "out", "IN\nTASK 1\nTASK 2\n");
 	}
 
 }
